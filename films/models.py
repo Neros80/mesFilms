7a59dc9679bbc563fa866c1ef8367efbaf086ae8
Antoine <a0415@hotmail.fr>
2022-11-05 18:03:50 +0100

    base de la page d'acceuil

--- films/models.py
+++ films/models.py
@@ -10,9 +10,12 @@ Films
 
 """
 
-class Product(models.Models):
-    title = models.CharFleid(max_lenght=128)
+class Product(models.Model):
+    title = models.CharField(max_length=128)
     date = models.DateField
-    director = models.CharField(max_lenght=128)
+    director = models.CharField(max_length=128)
     synopsis = models.TextField(blank=True)
     thumbnail = models.ImageField(upload_to="images", blank=True, null=True)
+
+    def __str__(self):
+        return self.title
--- films/views.py
+++ films/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from films.models import Product
+
+def index(request):
+    products = Product.objects.all()
+
+
+    return render(request, 'films/index.html', context={"products": products})
