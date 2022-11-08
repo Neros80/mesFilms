2c7ea9ad12a2b8e13f1fbe331eb78228c7169982
Neros80 <a0415@hotmail.fr>
2022-11-08 12:01:31 +0100

    debut formulaire d'ajout

--- films/views.py
+++ films/views.py
@@ -1,13 +1,26 @@
 
 from django.shortcuts import render, get_object_or_404
-from films.models import Movie
+from django.views.generic import View
+
+from films.models import Movie, Hall
+
 
 def index(request):
-    movie = Movie.objects.all()
-
-
+    movie = Movie.objects.all(),
     return render(request, 'films/index.html', context={"movies": movie})
 
 def movie_detail(request, slug):
     movie = get_object_or_404(Movie, slug=slug)
     return render(request, 'films/movie.html', context={"movie":movie} )
+
+def form(request):
+    hall = Hall.objects.all()
+    return render(request, 'films/form.html', context={"halls":hall} )
+
+class FormView(View):
+    
+    template_name = 'films/form.html'
+    
+    def get(self, request, *args, **kwargs):
+        context = {"message": "hello world"}
+        return render(request, self.template_name , context)
--- mesFilms/urls.py
+++ mesFilms/urls.py
@@ -4,10 +4,12 @@ from django.urls import path
 
 from films.views import index, movie_detail
 from mesFilms import settings
+from films import views
 
 urlpatterns = [
     path('', index, name='index'),
     path('film/<str:slug>', movie_detail, name='movie'),
+    path('form/', views.FormView.as_view(), name='form'),
     path('admin/', admin.site.urls),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
