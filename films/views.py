1a77ba7e6eae65a881df51513090f96f41fc6bae
Neros80 <a0415@hotmail.fr>
2022-11-09 14:29:48 +0100

    formulaire fonctionelle

--- films/views.py
+++ films/views.py
@@ -1,8 +1,9 @@
 
 from django.shortcuts import render, get_object_or_404
-
+from .form import MovieForm
 
 from films.models import Movie, Hall
+
 
 
 def index(request):
@@ -16,6 +17,73 @@ def movie_detail(request, slug):
 def form(request):
     halls = Hall.objects.all()
     return render(request, 'films/form.html', context={"halls":halls} )
+
+
+
+def movie_create(request):
+    form = MovieForm(request.POST, request.FILES or None)
+    if form.is_valid():
+        form.save()
+        form = MovieForm()
+        
+    return render(request, 'films/form.html', {'form':form})
+    
+# def add_movie(request, slug):
+#     if request.method == "POST":
+#         #traitement du form
+#         title = request.POST("title")
+#         slug = request.POST("slug")
+#         date = request.POST("date")
+#         director = request.POST("director")
+#         synopsis = request.POST("synopsis")
+#         thumbnail = request.POST("thumbnail")
+#         hall = request.POST("hall")
+        
+#     movie = get_object_or_404(Movie, slug=slug)
+#     new_movie, created = Movie.objects.create(
+#         title=title,
+#         slug=slug,
+#         date=date,
+#         director=director,
+#         synopsis=synopsis,
+#         thumbnail=thumbnail,
+#         hall=hall
+#         )   
+#     if created:
+#         new_movie.add(new_movie)
+#         new_movie.save()
+
+
+
+
+
+
+# def add_movie(request):
+#     if request.method == "POST":
+#         #traitement du form
+#         title = request.POST.get("title")
+#         slug = request.POST.get("slug")
+#         date = request.POST.get("date")
+#         director = request.POST.get("director")
+#         synopsis = request.POST.get("synopsis")
+#         thumbnail = request.POST.get("thumbnail")
+#         hall = request.POST.get("hall")
+        
+#         new_movie = Movie.objects.create(
+#             title=title,
+#             slug=slug,
+#             date=date,
+#             director=director,
+#             synopsis=synopsis,
+#             thumbnail=thumbnail,
+#             hall=hall
+#         )
+#         new_movie.save()
+        
+#     return render(request, 'films/form.html')
+
+
+
 
 # class FormView(View):
     
--- mesFilms/urls.py
+++ mesFilms/urls.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path
 
-from films.views import index, movie_detail, form
+from films.views import index, movie_detail, movie_create
 from mesFilms import settings
 
 urlpatterns = [
     path('', index, name='index'),
     path('film/<str:slug>', movie_detail, name='movie'),
-    path('form/', form, name='form'),
+    path('form/', movie_create, name='create'),
     path('admin/', admin.site.urls),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
