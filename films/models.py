5af2e4074c0e22f4ccb241c6072fda9aa8d66041
Neros80 <a0415@hotmail.fr>
2022-11-10 09:58:30 +0100

    maj models

--- films/models.py
+++ films/models.py
@@ -19,7 +19,7 @@ class Country(models.Model):
 
 class Studio(models.Model):
     name = models.CharField(max_length=128)
-    country = models.CharField(max_length=128)
+    country = models.ForeignKey(Country, on_delete=models.DO_NOTHING)
     
     def __str__(self):
         return f"{self.name}"
